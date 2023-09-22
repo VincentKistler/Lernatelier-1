e045b9614f764512dc24f6fcbba4b39b2b213e9d
VincentKistler <142605710+VincentKistler@users.noreply.github.com>
2023-09-22 16:17:39 +0200

    Add files via upload

---
+++ Zahleraten/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.ComponentModel.Design;
+
+namespace Zahleraten
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random zufallszahlengenerator = new Random();
+            int geheimeZahl = zufallszahlengenerator.Next(1, 20);
+            
+            bool geraten = false;
+            Console.WriteLine("Ich habe mir eine Zahl zwischen 1 und 20 ausgewählt, versuche diese zu erraten.");
+
+            while (!geraten)
+            {
+                Console.Write("Dein Tipp: ");
+                if (int.TryParse(Console.ReadLine(), out int tipp))
+                {
+
+
+                    if (tipp < geheimeZahl)
+                    {
+                        Console.WriteLine("Die gesuchte Zahl ist größer.");
+                    }
+                    else if (tipp > geheimeZahl)
+                    {
+                        Console.WriteLine("Die gesuchte Zahl ist kleiner.");
+                    }
+                    else
+                    {
+                        geraten = true;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ungültige Eingabe. Bitte gib eine Zahl zwischen 1 und 100 ein.");
+                }
+            }
+
+                    Console.WriteLine($"Glückwunsch! Du hast die geheime Zahl {geheimeZahl} erraten.");
+                }
+           }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+            
+        
+    
+
